061669b2302f4935f2fb44faaaf5b6551969dae5
Jim Mariel Ngasseu Ngonna <Jimmariel47@gmail.com>
2025-07-13 20:58:45 +0200

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,5 +1,5 @@
-if( process.argv[2] === undefined ){
-  console.log('process.argv[2]');
+if (process.argv[2] === undefined) {
+  console.log("No argument");
 } else {
-  console.log('No argument');
+  console.log(process.argv[2]);
 }
