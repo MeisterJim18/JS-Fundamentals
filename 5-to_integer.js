f316a4c8910db3fa0dc1cfdf2952945b29e95bb4
Jim Mariel Ngasseu Ngonna <Jimmariel47@gmail.com>
2025-07-13 21:39:51 +0200

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,5 +1,8 @@
-const arg = process.argv[2]; 
-const num = parseInt(arg);  
+const arg = process.argv[2];
+const num = parseInt(arg);
 
-if(!isNaN(num)) {          
-console.log(`My number: ${num}`);
+if (isNaN(num) || arg === undefined || arg.trim() === '') {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}
